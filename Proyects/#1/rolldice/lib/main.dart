4fc9d0373fbb4759d5b57217072b862511148c12
Valeria Caro <Valeria.Caro@outlook.com>
2023-08-31 22:26:27 -0500

    it works :P

--- Proyects/#1/rolldice/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import '';
-
-void main() {
-  runApp(MaterialApp(home: Text()));
-}
-
---
+++ Proyects/proyectone/android/app/src/main/kotlin/com/example/proyectone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proyectone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
