15a463b2e0ead1d5a7fec7e336c16e367ad73515
Valeria Caro <Valeria.Caro@outlook.com>
2023-08-31 23:58:56 -0500

    learn about widgets!

--- Proyects/proyectone/lib/main.dart
+++ Proyects/proyectone/lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hellooo')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.indigo,
+                Colors.indigoAccent,
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
-
