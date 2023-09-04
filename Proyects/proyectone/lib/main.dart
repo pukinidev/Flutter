815afc4bc440191bda8a8204c90a974fab827c1d
Valeria Caro <Valeria.Caro@outlook.com>
2023-09-03 21:51:10 -0500

    the return do not look

--- Proyects/proyectone/lib/main.dart
+++ Proyects/proyectone/lib/main.dart
@@ -4,20 +4,32 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.indigo,
-                Colors.indigoAccent,
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello'),
-          ),
-        ),
+        body: MyWidget(),
       ),
     ),
   );
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 221, 20, 218),
+              Color.fromARGB(255, 204, 19, 201),
+              Color.fromARGB(255, 128, 17, 126),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        );
+  }
 }
