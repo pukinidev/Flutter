ccedf91b0ca5724a4f6c14a99b44004668a688b0
Valeria Caro <Valeria.Caro@outlook.com>
2023-10-03 11:02:03 -0500

    google fonts

--- Proyects/quizapp/lib/answer_button.dart
+++ Proyects/quizapp/lib/answer_button.dart
@@ -22,7 +22,11 @@ class AnswerButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(40),
         ),
       ),
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+        
+      ),
     );
   }
 }
