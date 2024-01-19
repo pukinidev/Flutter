7d8ae879b2901a206c18048b58c651448c60cbba
Valeria Caro Ramirez <v.caro@uniandes.edu.co>
2024-01-18 22:12:59 -0500

    changes!

--- Projects/expensesapp/lib/widgets/expenses.dart
+++ Projects/expensesapp/lib/widgets/expenses.dart
@@ -26,20 +26,60 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpense() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
-      builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(
+        onAddExpense: _addExpense,
+      ),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Flutter ExpenseTracker',
-          style: TextStyle(
-            color: Colors.white,
-          ),
+          'ExpenseTracker',
         ),
         actions: [
           IconButton(
@@ -47,14 +87,9 @@ class _ExpensesState extends State<Expenses> {
             icon: const Icon(Icons.add),
           ),
         ],
-        backgroundColor: const Color.fromARGB(255, 69, 27, 78),
-        actionsIconTheme: const IconThemeData(color: Colors.white),
       ),
       body: Column(
-        children: [
-          const Text('Chart'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses))
-        ],
+        children: [const Text('Chart'), Expanded(child: mainContent)],
       ),
     );
   }
