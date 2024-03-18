179c1074a33e4a9db46d7039dbf5a4a98bd38daa
pukini <v.caro@uniandes.edu.co>
2024-03-18 18:32:55 -0500

    finally

--- Projects/expensesapp/lib/widgets/expenses.dart
+++ Projects/expensesapp/lib/widgets/expenses.dart
@@ -3,6 +3,7 @@ import 'package:expensesapp/models/expense.dart';
 import 'package:expensesapp/widgets/chart/chart.dart';
 import 'package:expensesapp/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -27,6 +28,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpense() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(
@@ -66,6 +68,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -89,9 +93,14 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
+      body: width < 600 ? Column(
         children: [
           Chart(expenses: _registeredExpenses),
+          Expanded(child: mainContent),
+        ],
+      ) : Row(
+        children: [
+          Expanded(child: Chart(expenses: _registeredExpenses)),
           Expanded(child: mainContent),
         ],
       ),
