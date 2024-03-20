910d77b1297274eaa855dbbdde00da075ab4dce6
pukini <v.caro@uniandes.edu.co>
2024-03-19 19:58:07 -0500

    changes and learn a lot of new stuff

--- Projects/mealsapp/lib/screens/categories.dart
+++ Projects/mealsapp/lib/screens/categories.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:mealsapp/data/dummy_data.dart';
+import 'package:mealsapp/models/category.dart';
+import 'package:mealsapp/screens/meals.dart';
 import 'package:mealsapp/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+  
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +31,6 @@ class CategoriesScreen extends StatelessWidget {
       ),
       body: GridView(
         padding: const EdgeInsets.all(24),
-        
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             childAspectRatio: 3 / 2,
@@ -30,8 +47,12 @@ class CategoriesScreen extends StatelessWidget {
             Option 2:
             Use the spread operator
           */
-          ...availableCategories
-              .map((category) => CategoryGridItem(category: category)),
+          ...availableCategories.map((category) => CategoryGridItem(
+                category: category,
+                onSelectedCategory: () {
+                  _selectCategory(context, category);
+                },
+              )),
 
           /*
             Option 3:
--- Projects/mealsapp/lib/widgets/category_grid_item.dart
+++ Projects/mealsapp/lib/widgets/category_grid_item.dart
@@ -5,43 +5,50 @@ class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({
     super.key,
     required this.category,
+    required this.onSelectedCategory,
   });
 
   final Category category;
+  final void Function() onSelectedCategory;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.55),
-            category.color.withOpacity(0.9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: category.color.withOpacity(0.08),
-            blurRadius: 10,
-            spreadRadius: 4,
-            offset: const Offset(0, 3),
+    return InkWell(
+      onTap: onSelectedCategory,
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-        ],
-      ),
-      child: Align(
-        alignment: Alignment.center,
-        child: Text(
-          category.title,
-          style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-                fontWeight: FontWeight.bold,
-                fontSize: 16
-                ,
-              ),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: category.color.withOpacity(0.08),
+              blurRadius: 10,
+              spreadRadius: 4,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16
+                  ,
+                ),
+          ),
         ),
       ),
     );
