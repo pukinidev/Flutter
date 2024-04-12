bb43ff1eb35d902210bd1d94a61138b4ea0bb987
pukini <v.caro@uniandes.edu.co>
2024-04-12 01:00:37 -0500

    I LOVE FLUTTER

--- Projects/shopping_app/lib/main.dart
+++ Projects/shopping_app/lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/widgets/grocery_list.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Flutter Groceries',
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 147, 229, 250),
+          brightness: Brightness.dark,
+          surface: const Color.fromARGB(255, 42, 51, 59),
         ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),
+      home: GroceryList(),
     );
   }
 }
---
+++ Projects/shopping_app/lib/widgets/grocery_list.dart
@@ -0,0 +1,112 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:shopping_app/data/categories.dart';
+import 'package:shopping_app/models/grocery_item.dart';
+import 'package:shopping_app/widgets/new_item.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'shoppingapp-31e68-default-rtdb.firebaseio.com', 'shopping_list.json');
+
+    final response = await http.get(url);
+    final Map<String,dynamic> data = json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in data.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (element) => element.value.title == item.value['category'])
+          .value;
+      _loadedItems.add(GroceryItem(
+        id: item.key,
+        name: item.value['name'],
+        quantity: item.value['quantity'],
+        category: category,
+      ));
+    }
+    setState(() {
+      _groceryItems = _loadedItems;
+    });
+  }
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(MaterialPageRoute(
+      builder: (context) => const NewItem(),
+    ));
+
+    if(newItem == null){
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items yet!'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            onDismissed: (direction) {
+              _removeItem(_groceryItems[index]);
+            },
+            key: ValueKey(_groceryItems[index].id),
+            child: ListTile(
+              title: Text(_groceryItems[index].name),
+              trailing: Text(_groceryItems[index].quantity.toString()),
+              leading: CircleAvatar(
+                backgroundColor: _groceryItems[index].category.color,
+                maxRadius: 10,
+                minRadius: 10,
+              ),
+            ),
+          );
+        },
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _addItem,
+          ),
+        ],
+      ),
+      body: content,
+    );
+  }
+}
